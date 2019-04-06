1d868ba22014d0ed86b8e4c215678fc93ef14ea1
hgd2018 <45419422+hgd2018@users.noreply.github.com>
2019-04-06 23:31:07 +0700

    Create collect-metrics.sh

---
+++ collect-metrics.sh
@@ -0,0 +1,46 @@
+source properties.env
+
+a1f=`ssh baru@$ip_alpha1 cat /var/log/secure|grep "Failed password"|wc -l`
+a1a=`ssh baru@$ip_alpha1 cat /var/log/secure|grep "Accepted password"|wc -l`
+no=`expr $a1f + $a1a`
+echo $no > /usr/share/nginx/html/nodes/$ip_alpha1.txt
+
+a2f=`ssh baru@$ip_alpha2 cat /var/log/secure|grep "Failed password"|wc -l`
+a2a=`ssh baru@$ip_alpha2 cat /var/log/secure|grep "Accepted password"|wc -l`
+no=`expr $a2f + $a2a`
+echo $no > /usr/share/nginx/html/nodes/$ip_alpha2.txt
+
+echo "<html>" > /usr/share/nginx/html/metrics.html
+echo "<head>" >> /usr/share/nginx/html/metrics.html
+echo "<style>" >> /usr/share/nginx/html/metrics.html
+echo "table {" >> /usr/share/nginx/html/metrics.html
+echo "  border-collapse: collapse;" >> /usr/share/nginx/html/metrics.html
+echo "  width: 20%;" >> /usr/share/nginx/html/metrics.html
+echo "}" >> /usr/share/nginx/html/metrics.html
+
+echo "th, td {" >> /usr/share/nginx/html/metrics.html
+echo "  text-align: left;" >> /usr/share/nginx/html/metrics.html
+echo "  padding: 8px;" >> /usr/share/nginx/html/metrics.html
+echo "}" >> /usr/share/nginx/html/metrics.html
+
+echo "tr:nth-child(even) {background-color: #f2f2f2;}" >> /usr/share/nginx/html/metrics.html
+echo "</style>" >> /usr/share/nginx/html/metrics.html
+echo "</head>" >> /usr/share/nginx/html/metrics.html
+echo "<title>Metrics SSH attempt login</title>" >> /usr/share/nginx/html/metrics.html
+echo "<body>" >> /usr/share/nginx/html/metrics.html
+echo "<table>" >> /usr/share/nginx/html/metrics.html
+echo "<tr>" >> /usr/share/nginx/html/metrics.html
+echo "<th>Metrics for ssh log-ins attempts</th>" >> /usr/share/nginx/html/metrics.html
+echo "</tr>" >> /usr/share/nginx/html/metrics.html
+
+files=`ls nodes`
+for no in ${files[*]}
+do
+   host=`echo "$no" | cut -f 1-4 -d "."`
+   att=`cat nodes/$no`
+   echo "<tr><td>* node"$host "had " $att "attempt</td></tr>" >> /usr/share/nginx/html/metrics.html
+done
+
+echo "</table>" >> /usr/share/nginx/html/metrics.html
+echo "<body>" >> /usr/share/nginx/html/metrics.html
+echo "</html>" >> /usr/share/nginx/html/metrics.html
