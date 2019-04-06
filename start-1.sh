1d08e6f3307c07b20d72f52efb563b43d4a69017
hgd2018 <45419422+hgd2018@users.noreply.github.com>
2019-04-07 00:33:09 +0700

    Update start-1.sh

--- start-1.sh
+++ start-1.sh
@@ -2,7 +2,7 @@
 chmod +x *.sh
 
 #Install nginx package
-echo -e "[nginx]\nname=nginx repo\nbaseurl=http://nginx.org/packages/mainline/centos/7/$basearch/\ngpgcheck=0\nenabled=1" \ > /etc/yum.repos.d/nginx.repo
+cp nginx.repo /etc/yum.repos.d/nginx.repo
 yum -y install nginx
 
 #Create nodes directory
